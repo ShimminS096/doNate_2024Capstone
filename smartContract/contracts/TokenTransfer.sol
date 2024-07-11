1c1fa44dc684c5b43f34b14596cf67ee98b5e1db
lawliet0424 <113508209+lawliet0424@users.noreply.github.com>
2024-07-12 01:03:44 +0900

    Refactor, Comment : 변수 이름, 주석 변경
    -TokenTransfer.sol
    
    Before : 20240712_17:02
    
    Note : 변수명 donatee -> benficiary로 변경, 주석 형식 변경

--- smartContract/contracts/TokenTransfer.sol
+++ smartContract/contracts/TokenTransfer.sol
@@ -15,9 +15,9 @@ contract TokenTransfer {
 /*
     Function name: sendTokensFromDonate
     Summary: 토큰을 전송하는 함수 (DonateBank => donator)
-    parameter: 총 2개 | address donator, uint256 amount
-        donator; 기부자 주소
-        amount; 전송할 토큰량
+    parameter: 총 2개
+        address donator; 기부자 주소
+        uint256 amount; 전송할 토큰량
     Return: 성공 여부 변수
     Caller: 없음
     Date: 2024.07.11
@@ -30,28 +30,27 @@ contract TokenTransfer {
     }
 
 /*
-    Function name: sendTokensToDonatee
-    Summary: 토큰을 전송하는 함수 (donator => donatee)
-    parameter: 총 2개 | address donator, uint256 amount
-        donatee; 수혜자 주소
-        amount; 전송할 토큰량
+    Function name: sendTokensTobenificiary
+    Summary: 토큰을 전송하는 함수 (donator => benificiary)
+    parameter: 총 2개
+        address benificiary; 수혜자 주소
+        uint256 amount; 전송할 토큰량
     Return: 성공 여부 변수
     Caller: 없음
     Date: 2024.07.11
     Write by: 심민서
 */
-    function sendTokensToDonatee(address donatee, uint256 amount) public returns (bool success){
+    function sendTokensToBenificiary(address benificiary, uint256 amount) public returns (bool success){
         require(donateTokenBank.balances(msg.sender) >= amount);
-        donateTokenBank.transfer(donatee, amount);
+        donateTokenBank.transfer(benificiary, amount);
         return true;
     }
 
 /*
     Function name: sendTokensToBank
-    Summary: 토큰을 전송하는 함수 (donator => donatee)
-    parameter: 총 1개 | address donator, uint256 amount
-        donatee; 수혜자 주소
-        amount; 전송할 토큰량
+    Summary: 토큰을 전송하는 함수 (beneficiary => DonateBank)
+    parameter: 총 1개
+        uint256 amount; 전송할 토큰량
     Return: 성공 여부 변수
     Caller: 없음
     Date: 2024.07.11
