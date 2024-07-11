d275473187e38f1c7b4bb76fd232bacc6ee1f1f8
lawliet0424 <113508209+lawliet0424@users.noreply.github.com>
2024-07-11 20:59:05 +0900

    Refactor, Comment :  주석 변경DonateTokenBank.sol

--- smartContract/contracts/DonateTokenBank.sol
+++ smartContract/contracts/DonateTokenBank.sol
@@ -3,10 +3,11 @@ pragma solidity ^0.8.0;
 
 contract DonateTokenBank {
     mapping(address => uint256) public balances;
+    uint256 public _initialSupply = 1000000;
     uint256 public totalSupply; // 토큰 총 발행량
     address public owner;
 
-    constructor(uint256 _initialSupply) {
+    constructor() {
         totalSupply = _initialSupply;
         balances[msg.sender] = _initialSupply;
         owner = msg.sender;
@@ -15,8 +16,8 @@ contract DonateTokenBank {
 /*
     Function name: additional_mint
     Summary: 토큰 추가 발행 함수
-    parameter: 총 1개 | uint256 amount
-        amount; 추가로 발행할 토큰량
+    parameter: 총 1개
+        uint256 amount; 추가로 발행할 토큰량
     Return: 성공 여부 변수
     Caller: 없음
     Date: 2024.07.11
@@ -31,9 +32,9 @@ contract DonateTokenBank {
 /*
     Function name: transfer
     Summary: 토큰 전송 함수
-    parameter: 총 2개 | address recipient, uint256 amount
-        recipient; 수신자 주소
-        amount; 전송할 토큰량
+    parameter: 총 2개
+        address recipient; 수신자 주소
+        uint256 amount; 전송할 토큰량
     Return: 성공 여부 변수
     Caller: sendTokens()
     Date: 2024.07.11
