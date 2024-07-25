fe7bb4a0a76188e8a6fa8ea085b66d2c19734889
JeongsuGil <lucy2468@naver.com>
2024-07-25 21:45:47 +0900

    Fix: beneficiarydetailpage에서 Auth 제거
    Before: 240725_16:50

--- donate/src/frontend/src/App.jsx
+++ donate/src/frontend/src/App.jsx
@@ -49,7 +49,7 @@ function App() {
           />
           <Route
             path="/beneficiarydetailpage"
-            element={<ProtectedRoute element={<BeneficiaryDetailPage />} />}
+            element={<BeneficiaryDetailPage />}
           />
           <Route
             path="/mypage"
